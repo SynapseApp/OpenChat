20e1823fec8298fde38a39fb8a1fe7a810e53417
SynapseApp <135055031+SynapseApp@users.noreply.github.com>
2024-09-06 17:38:20 +0530

    Update db.startup.ts

--- server/src/startup/db.startup.ts
+++ server/src/startup/db.startup.ts
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 import config from "../utils/config";
 
 export function initDb() {
+	const URI = config.DB_URI || "mongodb://localhost:27017/test";
 	mongoose
-		.connect(config.DB_URI || "mongodb://localhost:27017/test")
+		.connect(URI)
 		.then((db) => {
-			console.log("Connected to MongoDB");
+			console.log("Connected to MongoDB", URI);
 		})
 		.catch((err) => {
 			console.log(err);
