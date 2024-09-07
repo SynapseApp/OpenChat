17b56a2316f7da83e6f5dd1780fbc20c0bd621c2
SynapseApp <135055031+SynapseApp@users.noreply.github.com>
2024-09-07 08:58:23 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -34,7 +34,7 @@ app.use(
 		store,
 		cookie: {
 			maxAge: 1000 * 60 * 60 * 24 * 28, // Set the maximum age of the session cookie to 1 day
-			secure: config.NODE_ENV === "production", // Use secure cookies in production
+			secure: true, // Use secure cookies in production
       			sameSite: "none", // Allows cross-origin cookies
       			httpOnly: true, // Prevents client-side access to cookies
 		},
